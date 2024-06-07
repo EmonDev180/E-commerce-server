ac3596399abbb5128944a75c2aae90de578d7047
Emon Dev180 <shuvroemon.cse@gmail.com>
2024-06-07 18:30:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,286 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const  jwt = require('jsonwebtoken');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+//middleWere
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stvj7tw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('E-commerceDB').collection('products')
+    const cartCollection = client.db('E-commerceDB').collection('carts')
+    const userCollection = client.db('E-commerceDB').collection('users')
+    
+    // products 
+    app.get('/products',async(req,res) => {
+      const result = await productCollection.find().toArray();
+
+      res.send(result)
+    })
+
+    app.get('/product/:id',async(req,res) => {
+
+
+      const id = req.params.id;
+
+      const query = {_id: new ObjectId(id)}
+
+      const result = await productCollection.findOne(query)
+
+      res.send(result)
+      
+
+
+    })
+
+    app.post('/products',async(req,res) => {
+
+      const product = req.body;
+
+      const result = await productCollection.insertOne(product);
+
+      res.send(result);
+
+    })
+    app.delete('/products/:id',async(req,res) => {
+
+      const id = req.params.id;
+
+      console.log(id)
+
+      const query = {_id: new ObjectId(id)}
+
+      const result = await productCollection.deleteOne(query)
+
+      res.send(result);
+
+
+
+
+
+
+
+    })
+
+
+     // jwt related api
+
+     app.post('/jwt',async(req,res) => {
+
+      const user = req.body;
+
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"1h"
+      });
+      res.send({token});
+
+    })
+    // middleWere
+    const verifyToken = (req,res,next) => {
+      
+      console.log('insite varify token',req.headers.authorization);
+      
+      if(!req.headers.authorization){
+
+        return res.status(401).send({message:'forbiden access'})
+
+      }
+
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+          if(error){
+            return res.status(401).send({message : 'forbiden access'})
+          }
+          req.decoded = decoded
+        
+      })
+      next()
+  
+
+    
+
+    }
+
+
+    
+        // use varify admin after varifyToken 
+
+
+        const verifyAdmin = async(req,res,next) => {
+            
+          const email = req.decoded.email;
+
+          const query = {email:email};
+
+          const user = await userCollection.findOne(query);
+          
+          const isAdmin = user ?.role === 'admin';
+
+          if(!isAdmin){
+              return res.status(403).send({message:'forbiden access'})
+          }
+          next();
+
+
+
+      }
+
+   
+
+
+
+    
+
+    // user collection 
+
+    app.get('/users',verifyToken,verifyAdmin,async(req,res) => {
+
+
+      const result = await userCollection.find().toArray();
+      
+      res.send(result);
+
+    })
+    
+
+    app.get('/users/admin/:email',verifyToken,async(req,res) => {
+      const email = req.params.email;
+
+      if(email !== req.decoded.email){
+          return res.status(403).send({message:"unauthorized"})
+      }
+
+      const query = {email:email};
+
+      const user = await userCollection.findOne(query);
+
+      let admin = false;
+
+      if(user){
+          admin = user?.role === 'admin';
+      }
+
+      res.send({ admin });
+
+
+  })
+
+  app.post('/users', async (req, res) => {
+    const user = req.body;
+
+    // insert email if user does not exists  
+    // 
+
+    const query = { email: user.email }
+
+    const existingUser = await userCollection.findOne(query)
+
+    if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+    }
+
+    const result = await userCollection.insertOne(user)
+
+    res.send(result);
+
+
+})
+
+    app.delete('/users/:id',async(req,res) => {
+
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
+    app.patch('/users/admin/:id',async(req,res) => {
+
+      const id = req.params.id;
+
+      const filter = {_id: new ObjectId(id)}
+
+      const updatedDoc = {
+        $set:{
+          role:'admin'
+
+        }
+
+      }
+
+      const result = await userCollection.updateOne(filter,updatedDoc)
+      res.send(result);
+
+    } )
+
+    // cart collection 
+
+    app.get('/carts',async(req,res) => {
+
+      const email = req.query.email;
+      const query = {email:email};
+      const result = await cartCollection.find(query).toArray();
+
+      res.send(result)
+    })
+
+
+    app.post('/carts',async(req,res) => {
+      const cartItem = req.body;
+
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
+    app.delete('/carts/:id',async(req,res) => {
+
+      const id = req.params.id;
+      const query  = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+      
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res) => {
+    res.send('shop is runnning')
+})
+
+app.listen(port, () => {
+    console.log(`shop is port ${port}`);
+})
